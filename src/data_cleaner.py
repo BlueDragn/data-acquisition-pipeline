e5b167a61ad3b39dbf61fed79ddbd34a9aad103f
BlueDragn <76735748+BlueDragn@users.noreply.github.com>
2026-06-07 23:10:23 +0530

    feat: implement data cleaing module

---
+++ src/data_cleaner.py
@@ -0,0 +1,33 @@
+import pandas as pd
+def clean_posts(posts):
+    df = pd.DataFrame(posts)
+
+    df.dropna(inplace=True)
+
+    df = df.reset_index(drop=True)
+    return df
+
+
+if __name__ == "__main__":
+    sample_data = {
+            "userId": [1, 1, None],
+            "id": [1, 1, 2],
+            "title":["Test", "Another Test", None],
+            "body": ["Test Body", "Another Test Body", None]
+        }
+
+
+
+df = pd.DataFrame(sample_data)
+
+print("Before cleaning:")
+print(df)
+
+cleaned_df = clean_posts(sample_data)
+
+print("\nAfter cleaning:")
+print(cleaned_df)
+
+
+
+
